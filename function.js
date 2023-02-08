778f7581317350facc6aeb32b5c8f509b06b3c2b
Juan Manuel Silvani Marchegiani <124717368+jm-silvani-marchegiani@users.noreply.github.com>
2023-02-08 00:22:30 -0300

    Delete function.js

--- function.js
+++
@@ -1,8 +0,0 @@
-function ocultarFormulario(){
-    var formulario = document.getElementById("Formulario");
-    if (formulario.style.display == "block") {
-        formulario.style.display = "none";
-    } else if (formulario.style.display == "none") {
-		formulario.style.display = "block";
-	}
-}
